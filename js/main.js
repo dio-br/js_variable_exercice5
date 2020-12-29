a7c1ea4d51be61a10727a583b0b908582bc4714c
Mohammed <el-med7@outlook.com>
2020-12-29 10:18:10 +0100

    exo5

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Exercice 1
+let nombre1 = 10;
+let nombre2 = 2;
+let addition = `${nombre1} + ${nombre2} = ${nombre1 + nombre2}`;
+// console.log(addition);
+
+// console.log(`${nombre1} - ${nombre2} = ${nombre1 - nombre2}`);
+// console.log(`${nombre1} * ${nombre2} = ${nombre1 * nombre2}`);
+// console.log(`${nombre1} / ${nombre2} = ${nombre1 / nombre2}`);
+
+// Exercice 2 
+let prenom = prompt('Ton prénom ?');
+let age = parseInt(prompt('Ton âge ?'));
+// console.log(`${prenom} a ${age} ans cette année`);
+// console.log(typeof age);
+console.log(`${prenom} a ${age + 3} ans dans 3 ans`);
